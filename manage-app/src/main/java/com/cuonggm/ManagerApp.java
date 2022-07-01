b58f61bf06b297ebf120aa348ac04603822bc148
= <cuonggm4895@gmail.com>
2022-07-01 21:33:38 +0700

    Adjust trigger

---
+++ manage-app/src/main/java/com/cuonggm/ManagerApp.java
@@ -0,0 +1,31 @@
+package com.cuonggm;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Hello world!
+ */
+public final class ManagerApp {
+    private ManagerApp() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Start App");
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+        File file = new File("/home/ssm-user/" + now.format(formatter) + ".txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("End App");
+    }
+}
--- trigger/src/main/java/com/cuonggm/Trigger.java
+++ trigger/src/main/java/com/cuonggm/Trigger.java
@@ -1,10 +1,18 @@
 package com.cuonggm;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ssm.SsmClient;
+import software.amazon.awssdk.services.ssm.model.SendCommandRequest;
 
 /**
  * App
@@ -13,17 +21,68 @@ public class Trigger implements RequestHandler<SQSEvent, Void> {
 
     @Override
     public Void handleRequest(SQSEvent input, Context context) {
+        // Get Env Vars
+        String envCommand = System.getenv("command");
+        // String envAccessKeyId = System.getProperty("accessKeyId");
+        // String envSecretAccessKey = System.getenv("secretAccessKey");
+        String envInstanceId = System.getenv("instanceId");
+
+        // Check message
         List<SQSMessage> messages = input.getRecords();
-        System.out.println("Count of Message: " + messages.size());
-        for(SQSMessage message : messages) {
-            System.out.println("Message Name: " + message.getBody());
-            System.out.println("Count of Attributes: " + message.getAttributes().size());
-            for(String key : message.getAttributes().keySet()) {
-                String value = message.getAttributes().get(key);
-                System.out.println(String.format("Key=%s,value=%s", key, value));
+        if (messages != null) {
+            System.out.println("Count of Message: " + messages.size());
+            for (SQSMessage message : messages) {
+                System.out.println("Message Name: " + message.getBody());
+                System.out.println("Count of Attributes: " + message.getAttributes().size());
+                for (String key : message.getAttributes().keySet()) {
+                    String value = message.getAttributes().get(key);
+                    System.out.println(String.format("Key=%s,value=%s", key, value));
+                }
             }
         }
+
+        // Start App Log
+        System.out.println("Start App");
+        // Store Credentials
+        // System.out.println("Storing accessKeyId and secretAccessKey");
+        // Set accessKeyId
+        // System.setProperty("aws.accessKeyId", envAccessKeyId);
+        // Set secretAccessKey
+        // System.setProperty("aws.secretAccessKey", envSecretAccessKey);
+        // System.out.println("Store keypair successfully");
+        // Create Ec2Client
+        // Ec2Client client = Ec2Client.builder()
+        // .region(Region.US_EAST_1)
+        // .credentialsProvider(SystemPropertyCredentialsProvider.create())
+        // .build();
+
+        SsmClient ssmClient = SsmClient.builder()
+                .region(Region.US_EAST_1)
+                // .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .build();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
+        LocalDateTime now = LocalDateTime.now();
+
+        Map<String, List<String>> params = new HashMap<>();
+        List<String> paramContent = new ArrayList<>();
+        params.put("commands", paramContent);
+
+        // Set command content
+        paramContent.add(envCommand);
+
+        SendCommandRequest sendCommandRequest = SendCommandRequest.builder()
+                .documentName("AWS-RunShellScript")
+                .instanceIds(envInstanceId)
+                .comment("Run at" + now.format(formatter))
+                .parameters(params)
+                .build();
+        ssmClient.sendCommand(sendCommandRequest);
+        System.out.println(String.format("Ran command '%s' in instance '%s' at '%s'", envCommand, envInstanceId,
+                now.format(formatter)));
+        System.out.println("End App");
+
         return null;
     }
-    
+
 }
